c4ea49404ee246c7e2b2a7360cd9a09da3ba03e9
michael <2018118090@ub.edu.bz>
2023-03-29 22:28:15 -0600

    added the first migration for public users along with required extension

---
+++ backend/migrations/000001_create_public_user_table.down.sql
@@ -0,0 +1,5 @@
+-- Filename: BIOAFF/backend/migrations/000001_create_public_user_table.down.sql
+
+-- removing citext extension
+drop extension if exists citext;
+
---
+++ backend/migrations/000001_create_public_user_table.up.sql
@@ -0,0 +1,10 @@
+-- Filename: BIOAFF/backend/migrations/000001_create_public_user_table.up.sql
+
+--creating extension for the email type
+create extension if not exists citext;
+
+create table if not exists public_user(
+    id serial primary key,
+    email citext unique not null,
+    pu_password bytea not null,
+)
