a5e7f2d22461b9787d31b86235d1578f4cd895f8
michael <2018118090@ub.edu.bz>
2023-03-30 20:21:52 -0600

    added a misisng depency

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -10,6 +10,7 @@ import (
 	"sync"
 	"time"
 
+	_ "github.com/lib/pq"
 	"se_group1.net/internal/jsonlog"
 )
 
@@ -60,7 +61,7 @@ func main() {
 	//db and webserver flags
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development | staging | production)")
-	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("$BIOAFF_DB_DSN"), "PostgresSQL DSN")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("BIOAFF_DB_DSN"), "PostgresSQL DSN")
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQl max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.StringVar(&cfg.db.maxIdelTime, "db-max-idle-time", "15m", "PostgreSQl max connection idle time")
--- backend/go.mod
+++ backend/go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/lib/pq v1.10.7 // indirect
 	golang.org/x/crypto v0.7.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
 )
