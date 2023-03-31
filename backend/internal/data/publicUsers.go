3c199d7e88d2d37fb7bea57a770ae55ebf467628
michael <2018118090@ub.edu.bz>
2023-03-31 01:16:34 -0600

    finished with the publicUser model, moving unto handlers

--- backend/internal/data/publicUsers.go
+++ backend/internal/data/publicUsers.go
@@ -2,8 +2,10 @@
 package data
 
 import (
+	"context"
 	"database/sql"
 	"errors"
+	"time"
 
 	"golang.org/x/crypto/bcrypt"
 	"se_group1.net/internal/validator"
@@ -13,26 +15,12 @@ var (
 	ErrDuplicateEmail = errors.New("duplicate email")
 )
 
-// Declaring an AnonymousUser, no id, no name, no email, no password
-var AnonymousUser = &PublicUser{}
+//Password stuff
 
 // creating a password type
 type password struct {
 	plaintext *string
 	hash      []byte
-}
-
-// creaing the PublicUser model
-type PublicUser struct {
-	ID       int64    `json:"id"`
-	Name     string   `json:"name"`
-	Email    string   `json:"email"`
-	Password password `json:"-"`
-}
-
-// checking if a user is anonymous
-func (u *PublicUser) IsAnonymous() bool {
-	return u == AnonymousUser
 }
 
 // Set() - stores the hash of the plaintext pasword
@@ -63,18 +51,141 @@ func (p *password) Matches(plaintextPassword string) (bool, error) {
 	return true, nil
 }
 
+//PublicUser stuff
+
+// creaing the PublicUser model
+type PublicUser struct {
+	ID        int64     `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  password  `json:"-"`
+	Activated bool      `json:"activated"`
+	CreatedAt time.Time `json:"created_at"`
+	Version   int       `json:"-"`
+}
+
+// Declaring an AnonymousUser, no id, no name, no email, no password
+var AnonymousUser = &PublicUser{}
+
+// checking if a user is anonymous
+func (u *PublicUser) IsAnonymous() bool {
+	return u == AnonymousUser
+}
+
+// creating our publicuser model
+type PublicUserModel struct {
+	DB *sql.DB
+}
+
+//CRUD functions will be made later
+
+// Insert() - creates a new publicuser on the database
+func (m PublicUserModel) Insert(p_user *PublicUser) error {
+	//creating the query
+	query := `
+		insert into public_user (name, email, pu_password, activated)
+		values ($1, $2, $3, $4)
+		returning id, created_at, version
+	`
+
+	//creating the context for this function
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	//collecting the data fields into a slice for query insertion
+	args := []interface{}{p_user.Name, p_user.Email, p_user.Password.hash, p_user.Activated}
+
+	//executing the query
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&p_user.ID, &p_user.CreatedAt, &p_user.Version)
+	if err != nil {
+		switch {
+		case err.Error() == `pq: duplicate key value violates unique constraint "public_user_email_key"`:
+			return ErrDuplicateEmail
+		default:
+			return err
+		}
+	}
+	return nil
+}
+
+// GetByEmail() - get's the publicuser based on their email
+func (m PublicUserModel) GetByEmail(email string) (*PublicUser, error) {
+	query := `
+		select id, name, email, pu_password, activated, created_at, version
+		from public_user
+		where email = $1
+	`
+
+	var p_user PublicUser
+
+	//creating the context for this function
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, query, email).Scan(
+		&p_user.ID,
+		&p_user.Name,
+		&p_user.Email,
+		&p_user.Password.hash,
+		&p_user.Activated,
+		&p_user.Version,
+	)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+	return &p_user, nil
+}
+
+// Update() - updates the public_user record if necessary
+func (m PublicUserModel) Update(p_user *PublicUser) error {
+	query := `
+		update public_user
+		set name = $1, email = $2, pu_password = $3, activated = $4, version = version + 1
+		where id = $5 and version = $6
+		returning version
+	`
+
+	//creating the context for this function
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	//collecting the data fields into a slice for query insertion
+	args := []interface{}{p_user.Name, p_user.Email, p_user.Password, p_user.Activated, p_user.ID, p_user.Version}
+
+	//running the query
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&p_user.Version)
+	if err != nil {
+		switch {
+		case err.Error() == `pq: duplicate key value violates unique constraint "public_user_email_key"`:
+			return ErrDuplicateEmail
+		default:
+			return err
+		}
+	}
+	return nil
+}
+
+//Validation stuff
+
 // Validating client requests
 func ValidateEmail(v *validator.Validator, email string) {
 	v.Check(email != "", "email", "must be provided")
 	v.Check(validator.Matches(email, validator.EmailRX), "email", "must be a valid email address")
 }
 
+// validating the password
 func ValidatePasswordPlaintex(v *validator.Validator, password string) {
 	v.Check(password != "", "password", "must be provided")
 	v.Check(len(password) >= 8, "password", "must be atleast 8 bytes long")
 	v.Check(len(password) <= 72, "password", "must not be more than 72 bytes long")
 }
 
+// validating our user
 func ValidateUser(v *validator.Validator, publicUser *PublicUser) {
 	v.Check(publicUser.Name != "", "name", "must be provided")
 	v.Check(len(publicUser.Name) <= 500, "name", "must not be more than 500 bytes long")
@@ -92,9 +203,4 @@ func ValidateUser(v *validator.Validator, publicUser *PublicUser) {
 	}
 }
 
-// creating our publicuser model
-type PublicUserModel struct {
-	DB *sql.DB
-}
-
-//crud functions will be made later
+//tokens will be added later
